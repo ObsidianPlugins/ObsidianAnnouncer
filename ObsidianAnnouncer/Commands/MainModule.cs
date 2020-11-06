a759b9833b59d9633af2ecbbdaf4a14b59417ba0
roxxel <64275777+roxxel@users.noreply.github.com>
2020-11-07 00:45:24 +0300

    Update MainModule.cs

--- ObsidianAnnouncer/Commands/MainModule.cs
+++ ObsidianAnnouncer/Commands/MainModule.cs
@@ -14,10 +14,11 @@ namespace ObsidianAnnouncer.Commands
 {
     public class MainModule : BaseCommandClass
     {
+        //For testing purposes, will be deleted soon
         [Command("test")]
         public async Task TestCmd(ObsidianContext ctx)
         {
-            var json = "[[{\"text\":\" 12321gdsagdashgdahdghhds\",\"clickEvent\":{\"action\":\"open_url\", \"value\":\"https:\\/\\/www.youtube.com\\/watch?v=dQw4w9WgXc\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"100% NOT A RICKROLL\"}}]]";
+            var json = "[[{\"text\":\" 12321gdsagdashgdahdghhds\",\"clickEvent\":{\"action\":\"open_url\", \"value\":\"https:\\/\\/www.youtube.com\\/watch?v=dQw4w9WgXcQ\"},\"hoverEvent\":{\"action\":\"show_text\",\"value\":\"100% NOT A RICKROLL\"}}]]";
             var msgs = JsonConvert.DeserializeObject<List<List<Message>>>(json);
             foreach (var msg in msgs)
             {
